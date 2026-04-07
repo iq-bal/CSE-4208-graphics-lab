ebfb3ff60186d4a23755b6fead43d55d2ab55061
Iqbal Mahamud <96818871+iq-bal@users.noreply.github.com>
2026-04-07 23:57:18 +0600

    update: k key press

--- project/main.cpp
+++ project/main.cpp
@@ -1112,7 +1112,7 @@ void processInput(GLFWwindow *window) {
   if (glfwGetKey(window, GLFW_KEY_K) == GLFW_PRESS) {
     if (!kKeyPressed) {
       inExterior = true;
-      camera.Position = glm::vec3(0.0f, CAMERA_EYE_HEIGHT, -128.0f);
+      camera.Position = glm::vec3(0.0f, CAMERA_EYE_HEIGHT, -235.0f);
       camera.Yaw = -90.0f;
       camera.Pitch = -5.0f;
       camera.updateCameraVectors();
